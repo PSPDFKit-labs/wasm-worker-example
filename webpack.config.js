ebdfb5fbf2a16d3b979a93ef7c6ee11432bf3857
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2024-09-02 14:13:17 +0500

    Update project

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 module.exports = {
-  entry: "./index.js",
-  output: {
-    filename: "bundle.js",
-    publicPath: "dist/",
-    globalObject: 'typeof self !== "object" ? self : this'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.worker\.js/,
-        use: {
-          loader: "worker-loader",
-          options: { fallback: true }
-        }
-      },
-      {
-        test: /\.wasm$/,
-        type:
-          "javascript/auto" /** this disabled webpacks default handling of wasm */,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "wasm/[name].[hash].[ext]",
-              publicPath: "/dist/"
-            }
-          }
-        ]
-      }
-    ]
-  }
+	mode: 'development', // Set to "development", "production", or "none"
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js',
+		publicPath: 'dist/',
+		globalObject: 'typeof self !== "object" ? self : this',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.worker\.js$/,
+				use: {
+					loader: 'worker-loader',
+					options: {
+						filename: '[name].worker.js', // Customize the filename for the worker if needed
+						publicPath: 'dist/', // Specify the public path if necessary
+						esModule: false, // Set esModule to false for compatibility
+					},
+				},
+			},
+			{
+				test: /\.wasm$/,
+				type: 'javascript/auto', // This disables webpack's default handling of wasm
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'wasm/[name].[hash].[ext]',
+							publicPath: '/dist/',
+						},
+					},
+				],
+			},
+		],
+	},
 };
